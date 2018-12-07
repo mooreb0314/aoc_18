3e60e3b029785ec1057e2d4891e8463a36d0064f
mooreb0314 <mooreb0314@gmail.com>
2018-12-07 13:18:15 -0500

    Added solution to part 1

--- day_4/solution_1.py
+++ day_4/solution_1.py
@@ -4,41 +4,54 @@ with open('input.txt') as file:
 lines = data.splitlines()
 lines.sort()
 
-time_card = [0 for x in range(60)]
+guards = []
 
-curr_guard = 0
-sleeping = False
-start = 0
-end = 0
-chosen_guard = 353
 for line in lines:
-    #print(line)
     if line.split("] ")[1].startswith("Guard"):
-        curr_guard = int(line.split("] ")[1].split(" ")[1].split("#")[1])
-        if curr_guard == chosen_guard:
-            print(line)
+        guard = int(line.split("] ")[1].split(" ")[1].split("#")[1])
+        if guard not in guards:
+            guards.append(guard)
+
+solution = {"Guard": 0 , "Time" : 0, "Minute": 0}
+
+for guard in guards:
+    chosen_guard = guard
+    sleeping = False
+    time_card = [0 for x in range(60)]
+
+    for line in lines:
+        #print(line)
+        if line.split("] ")[1].startswith("Guard"):
+            curr_guard = int(line.split("] ")[1].split(" ")[1].split("#")[1])
+            if curr_guard == chosen_guard:
+                i = 1
+            else:
+                start = 0
+                end = 0
+
+        elif int(line.split("] ")[1].startswith("wakes")):
+            if curr_guard == chosen_guard:
+                #print(line)
+                end = int(line.split("] ")[0].split(" ")[1].split(":")[1])
+                #print("Start: " + str(start) + " End: " + str(end))
+                for i in range(start, end):
+                    time_card[i] += 1
+
+                #print(time_card)
+
         else:
-            start = 0
-            end = 0
 
-    elif int(line.split("] ")[1].startswith("wakes")):
-        if curr_guard == chosen_guard:
-            print(line)
-            end = int(line.split("] ")[0].split(" ")[1].split(":")[1])
-            print("Start: " + str(start) + " End: " + str(end))
-            for i in range(start, end):
-                time_card[i] += 1
+            if curr_guard == chosen_guard:
+                #print(line)
+                start = int(line.split("] ")[0].split(" ")[1].split(":")[1])
 
-            print(time_card)
+    if sum(time_card) > solution["Time"]:
+        solution["Guard"] = chosen_guard
+        solution["Time"] = sum(time_card)
+        solution["Minute"] = time_card.index(max(time_card))
 
-    else:
+print(solution)
 
-        if curr_guard == chosen_guard:
-            print(line)
-            start = int(line.split("] ")[0].split(" ")[1].split(":")[1])
-
-print(time_card.index(max(time_card)))
-print(max(time_card))
-print((time_card.index(max(time_card))) * chosen_guard)
+print(solution["Guard"] * solution["Minute"])
 
 
