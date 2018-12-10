7ca7eb906e4bc9780bfbebc0553310990a6e7247
mooreb0314 <mooreb0314@gmail.com>
2018-12-10 10:00:08 -0500

    Finished solution for part 2 on day 5

--- day_5/solution_2.py
+++ day_5/solution_2.py
@@ -6,24 +6,37 @@ di = {}
 for c in set(lower):
     di[c] = lower.count(c)
 
+print(di)
 #print(max(di.keys(), key=(lambda key: di[key])))
 
-data.replace(max(di.keys(), key=(lambda key: di[key])),"")
-data.replace(max(di.keys(), key=(lambda key: di[key])).upper(),"")
+#data.replace(max(di.keys(), key=(lambda key: di[key])),"")
+#data.replace(max(di.keys(), key=(lambda key: di[key])).upper(),"")
 
-chars = [c for c in data]
+low = 100000
+for k in di.keys():
+    #print(k)
+    tmp = data
+    tmp = tmp.replace(k,"")
+    tmp = tmp.replace(k.upper(),"")
 
-removed = True
-while removed:
-    removed = False
+    chars = [c for c in tmp]
+    i = 0
 
-    for i,_ in enumerate(chars):
-        if i < len(chars) - 1:
-            if abs(ord(chars[i]) - ord(chars[i+1])) == 32:
-                chars.pop(i)
-                chars.pop(i)
-                removed = True
-                break
+    while i < len(chars) - 1:
+
+        if abs(ord(chars[i]) - ord(chars[i+1])) == 32:
+            a = chars.pop(i)
+            b = chars.pop(i)
+
+            if i != 0:
+                i -= 1
+
+        else:
+            i += 1
+
+    if len(chars) < low:
+        low = len(chars)
+
+print(low)
 
 
-print(len(chars))
