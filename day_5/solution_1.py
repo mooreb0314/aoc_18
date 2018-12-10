fd4daa78185db67e34e572153d20d721ccea7b61
mooreb0314 <mooreb0314@gmail.com>
2018-12-10 09:43:25 -0500

    Optimized solution 1

--- day_5/solution_1.py
+++ day_5/solution_1.py
@@ -4,41 +4,18 @@ with open('input.txt') as file:
 #data = "dabAcCaCBAcCcaDA"
 chars = [c for c in data]
 
-'''
-removed = True
-while removed:
-    removed = False
-
-    for i,_ in enumerate(chars):
-        if i < len(chars) - 1:
-            if abs(ord(chars[i]) - ord(chars[i+1])) == 32:
-                a = chars.pop(i)
-                b = chars.pop(i)
-                print("Removed: " + a + b)
-                removed = True
-                break
-
-'''
-
 i = 0
-#print(chars)
 while i < len(chars) - 1:
-    #print(str(chars[i]) + " - " +  str(chars[i+1]) +  " = " + str(abs(ord(chars[i]) - ord(chars[i+1]))))
-    #print(chars[-10:])
 
     if abs(ord(chars[i]) - ord(chars[i+1])) == 32:
-        #print(chars.pop(i))
-        #print(chars.pop(i))
         a = chars.pop(i)
         b = chars.pop(i)
-        #print("Removed: " + a + b)
+
         if i != 0:
             i -= 1
-        #print(chars)
+
     else:
         i += 1
-
-
 
 print(''.join(chars))
 print(len(chars))
